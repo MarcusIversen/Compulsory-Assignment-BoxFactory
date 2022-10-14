bb368f1fea0b93c381c591e76862c338453e48d4
KristianHollaender <kristianhansen9@hotmail.dk>
2022-10-14 09:06:37 +0200

    Started worked on the frontend

--- API/Program.cs
+++ API/Program.cs
@@ -56,6 +56,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
